ac85c45884cf21052b2f16d9f7079a6d44377331
boven <boven.yan@gmail.com>
2016-07-19 21:50:06 -0400

    clone script updates

--- vmPrep/cloneVM.sh
+++ vmPrep/cloneVM.sh
@@ -1,9 +1,72 @@
 # usage:
 # ./run.sh $src $
 
-IMG_DIR=/var/lib/libvirt/images
-IMG_NAME=$2
+function print_help {
+    echo "Usage ./cloneVM.sh [Options]"
+    echo "  -h, --help      print this help msg"
+    echo "  -o, --origin    specify the orgin template"
+    echo "  -n, --name      specify the name of the cloned machine"
+    exit
+}
 
-sudo mkdir $IMG_DIR/$IMG_NAME
-sudo chown bovenyan:bovenyan $IMG_DIR/$IMG_NAME
-virt-clone --connect=qemu:///system -o $1 -n $IMG_NAME -f /var/lib/libvirt/images/$IMG_NAME/$IMG_NAME".gcow2"
+if [[ $# == 1 ]]; then
+    if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+        print_help
+        exit
+    fi  
+
+    echo "Unknown Option: "$1
+    print_help
+    exit
+fi
+
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+
+    case $key in
+        -h|--help)
+            print_help
+            exit
+            ;;
+    
+        -o|--origin)
+            ORIGIN=$2
+            shift
+            shift
+            ;;
+        -n|--name)
+            NAME=$2
+            shift
+            shift
+            ;;
+        *)
+            echo "Unkown Option:"$key
+            print_help
+    esac
+done
+
+if [[ -z $ORIGIN ]]; then
+    echo "Enter the name of the template"
+    read ORIGIN
+fi
+
+if [[ -z $NAME ]]; then
+    echo "Enter the name of the cloned machine"
+    read NAME
+fi 
+
+
+IMG_DIR=/var/lib/libvirt/images
+
+sudo mkdir -p $IMG_DIR/$NAME
+sudo chown $USER:$USER $IMG_DIR/$NAME
+virt-clone --connect=qemu:///system -o $ORIGIN -n $NAME -f /var/lib/libvirt/images/$NAME/$NAME".gcow2"
+
+echo "Custom the VM? (Y/n)"
+read choice
+if [ $choice == "n" ] || [ $choice == "N" ]; then
+    exit
+else
+    virsh edit $NAME
+fi
