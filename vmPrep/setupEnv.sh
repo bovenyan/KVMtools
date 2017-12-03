970b1267c1f29e44f07780e89d3cdc8d2972f704
Bo Yan <boven.yan@gmail.com>
2017-12-02 19:14:55 -0500

    add git install and settings

--- vmPrep/setupEnv.sh
+++ vmPrep/setupEnv.sh
@@ -1,9 +1,11 @@
 sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt-get update
 
-sudo apt-get install -y vim astyle
+sudo apt-get install -y vim astyle git
 sudo apt-get install -y build-essential cmake
 sudo apt-get install -y python-dev python3-dev
+
+git config --global push.default simple
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc $HOME/
